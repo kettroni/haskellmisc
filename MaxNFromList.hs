1ad797f94f28c1b94fa5e85c38dd93c189c0a084
Roni Kettunen <ronikettunen96@gmail.com>
2021-06-17 22:27:54 +0300

    Forgot to add this one also.

--- MaxNFromList.hs
+++ MaxNFromList.hs
@@ -1,2 +1,11 @@
 maxNFromList :: (Ord a) => [a] -> a -> [a]
 maxNFromList xs n = [ if x > n then n else x | x <- xs]
+
+maxNFromListRecursive :: (Ord a) => [a] -> a -> [a]
+maxNFromListRecursive [] _ = []
+maxNFromListRecursive (x:xs) n
+  | x > n     = n : rest
+  | otherwise = x : rest
+  where rest = maxNFromListRecursive xs n
+
+
